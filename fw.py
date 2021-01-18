fb30984025c786d1b68460a265f7642cdbe66a5b
bomberwaterman <bwman@hotmail.fr>
2021-01-18 22:28:36 +0000

    V1

--- fw.py
+++ fw.py
@@ -17,19 +17,45 @@ class Traitement:
 
     def OpenFile(self,nom_fichier):
         self.f = open(nom_fichier, "r")
+        #clear du fichier temporaire avant ouverture en Rw
+        self.ftemp = open("temp-file.txt","r+")
+        #self.ftemp.seek(0)                        
+        #self.ftemp.truncate()
+        
         
     def ReadLine(self):
       self.NbLignes = self.f.readlines()
       n = 1
       temp = []
+
+      #parcours du fichier source principal
       for line in self.NbLignes : 
         n+=1
-        temp = line.split("srcip=\"")
-        print(temp[1].split("\"",1))
+        #découpe des lignes délimiteur espace par défaut
+        temp = line.split()
 
-        for elt in temp :
-          print(elt)
+        print("ligne37 "+temp[15])
+
+        #écrire infos interessantes dans fichier temporaire
+        self.Info_concat=temp[15]+" "+temp[16]+ "\n"
+        self.ftemp.write(self.Info_concat)
+        #pour chaque ligne, je regarde si occurence existe dans le fichier temporaire
+        
+        lines = self.ftemp.readlines()
+        print(lines)
+        for line in lines :
+          if line == self.Info_concat:
+            print("ok")
+        
+
+        if temp[15] == "srcip=\"10.110.0.23\"" :
+            print("ok")
+        #for elt in temp :
+         # print(" salut " + elt)
       self.PrintN(n)
+      lines = self.ftemp.readlines()
+      for linet in lines:
+        print(linet.strip())
     
     def PrintN(self,N):
       print(N)
@@ -38,7 +64,7 @@ class Traitement:
 
 
 a = Traitement()
-a.OpenFile("import-fw.txt")
+a.OpenFile("import-fw-lite.txt")
 a.ReadLine()
 a.PrintN(42)
 
